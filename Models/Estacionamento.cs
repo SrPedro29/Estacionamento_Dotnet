4e5f0b24d96a9ec880e86385646ecf4914204f4f
SrPedro29 <pedroaugustocardoso.pac@gmail.com>
2024-09-24 21:08:31 -0300

    first commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_Estacionamento.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial { get; set; }
+        private decimal precoPorHora { get; set; }
+        private List<string?> veiculos = new List<string?>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora){
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a Placa do carro quer adicionar: ");
+            veiculos.Add(Console.ReadLine()?.ToUpper());
+            Console.WriteLine("Veículo adicionado!");
+            
+            Console.WriteLine($"Placa: {veiculos[veiculos.Count-1]}");
+
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a Placa do carro quer remover: ");
+            veiculos.Remove(Console.ReadLine()?.ToUpper());
+            Console.WriteLine("Veículo removido!");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o tempo que o veículo ficou estacionado: ");
+            decimal tempEstacionado = Convert.ToDecimal(Console.ReadLine());
+            decimal pagamento = precoInicial + (precoPorHora*tempEstacionado);
+            Console.WriteLine($"Valor do estacionamento foi:  R${pagamento}");
+        }
+
+
+        public void ListarCarros()
+        {
+            if (veiculos.Count != 0)
+            {
+                foreach (string? carros in veiculos)
+                {
+                    Console.WriteLine($"Placa: {carros}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados");
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Desafio_Estacionamento.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento! \n" +
+                    "Digite o valor inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite um número correspondente a ação que deseja: ");
+    Console.WriteLine("1 - Adicionar Veiculos");
+    Console.WriteLine("2 - Remover Veiculos");
+    Console.WriteLine("3 - Listar Veiculos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            Console.WriteLine();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            Console.WriteLine();
+            break;
+
+        case "3":
+            es.ListarCarros();
+            Console.WriteLine();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Comando Inválido, tente novamente com os número da tabela");
+            break;
+    }
+    Console.WriteLine("Digite qualquer tecla para continuar:");
+    Console.ReadLine();
+}
+Console.WriteLine("Programa encerrado, até a proxima!");
